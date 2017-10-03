df4c82a3d715d26c3d148e17bd80e232fc96521c
Abuzer Emre Osmanoğlu <aeosmanoglu@icloud.com>
2017-10-03 22:52:43 +0300

    Initial Commit

---
+++ WebApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebApp
+//
+//  Created by Abuzer Emre Osmanoğlu on 3.10.2017.
+//  Copyright © 2017 Abuzer Emre Osmanoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
